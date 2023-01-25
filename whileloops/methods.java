8cb94e54ef028a49a0539b75ca6e6453f9606a19
Ryan Hulke <61066664+ryanhulke@users.noreply.github.com>
2023-01-24 19:58:53 -0500

    Delete methods.java

--- whileloops/methods.java
+++
@@ -1,9 +0,0 @@
-package whileloops;
-
-public class methods {
-    public static void  powersOfTwo() {
-        for (int i = 2; i <= 10; i++){
-            System.out.print(Math.pow(2, i));
-        }
-    }
-}
